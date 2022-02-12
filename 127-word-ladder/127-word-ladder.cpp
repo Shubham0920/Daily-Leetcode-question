40f3a3e5568c723da500ba7a0bceac2c31af7860
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2022-02-12 17:30:26 +0530

    Time: 216 ms (52.10%), Space: 16.4 MB (36.15%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-    int ladderLength(string b, string e, vector<string>& l) {
-        int n=b.size();
-    string tot="abcdefghijklmnopqrstuvwxyz";
-
-         unordered_set<string>hp;
-         unordered_map<string,bool>vis;
+    int ladderLength(string b, string e, vector<string>& w) {
+        int n = b.size();
+        string tot = "abcdefghijklmnopqrstuvwxyz";
+        unordered_set<string> set;
+        unordered_map<string,bool> vis;
         
-        for(auto &i: l)
-        {
-            hp.insert(i);
+        for(auto &i:w){
+            set.insert(i);
         }
+        if(set.count(e)== 0)return 0;
         
-        if(hp.count(e)==0)return 0;
-        
-        queue<pair<string,int>>q;
+        queue<pair<string,int>> q;
         q.push({b,0});
+        vis[b] = true;
         
-        vis[b]=true;
-        
-        while(q.size())
-        {
-            auto it=q.front();
+        while(q.size()){
+            auto it = q.front();
             q.pop();
-            string s=it.first;
-            int dis=it.second;
-            if(s==e){
-                return dis+1;
-            }
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<tot.size();j++)
-                {
-                    string ao=s;
-                    ao[i]=tot[j];
-                    if(hp.count(ao) && vis[ao]!=true)
-                    {
-                        vis[ao]=true;
+            string s = it.first;
+            int dis = it.second;
+            if(s == e) return dis+1;
+            
+            for(int i=0;i<n;i++){
+                for(int j=0;j<tot.length();j++){
+                    string ao = s;
+                    ao[i] = tot[j];
+                    if(set.count(ao)!=0 && vis[ao] != true){
                         q.push({ao,dis+1});
+                        vis[ao] = true;
                     }
                 }
             }
-            
         }
         return 0;
-        
     }
 };
