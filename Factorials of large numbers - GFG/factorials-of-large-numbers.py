01cea6fba48cd81e7ffdead4b329aaf09f704310
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2022-02-23 16:23:55 +0530

    Added solution - LeetHub

---
+++ Factorials of large numbers - GFG/factorials-of-large-numbers.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def factorial(self, N):
+        #code here
+        #code here
+        a=[]
+        s=1
+        for i in range(1,N+1):
+            s=s*i
+        a.append(s)
+        
+        return  a
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.factorial(N);
+        for i in ans:
+            print(i,end="")
+        print()
+    
+# } Driver Code Ends
