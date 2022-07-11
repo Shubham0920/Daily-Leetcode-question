5f715a19f4a4b6bb7d2777697994aa33329dd65a
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2022-07-11 23:26:36 +0530

    Added solution - LeetHub

---
+++ Reorganize The Array - GFG/reorganize-the-array.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+int * Rearrange(int *arr,int n);
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n;cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	    int *b = Rearrange(a,n);
+	    for(int i=0;i<n;i++)
+	        cout << b[i] <<" ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}// } Driver Code Ends
+
+
+int * Rearrange(int *arr,int n){
+    // Complete the function
+    int i=0;
+    while(i<n){
+        if(arr[i]>=0 && arr[i] != i){
+            swap(arr[i],arr[arr[i]]);
+        }
+        else{
+            i++;
+        }
+    }
+    return arr;
+}
