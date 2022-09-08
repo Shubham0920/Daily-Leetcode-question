5f2e366e9e4f439f5e2794478b3d79a785120012
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2022-09-08 20:45:55 +0530

    Time: 16 ms (50.29%), Space: 11.2 MB (75.17%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     void findcombination(int ind,vector<int> &arr,int target,vector<vector<int>> &ans,vector<int> &temp){
-        if(ind == arr.size()){
-            if(target == 0){
-                ans.push_back(temp);
-            }
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+        if(arr.size() == ind){
             return;
         }
         if(arr[ind] <= target){
             temp.push_back(arr[ind]);
-            cout<<arr[ind]<<" ";
+            // cout<<arr[ind]<<" ";
             findcombination(ind,arr,target-arr[ind],ans,temp);
-            cout<<arr[ind]<<" ";
+            // cout<<arr[ind]<<" ";
             temp.pop_back();
         }
         findcombination(ind+1,arr,target,ans,temp);
