fe10e8d16f98c520904a749009e72dd1388ad840
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2023-08-14 14:36:22 +0530

    Added solution - LeetHub

---
+++ Maximum Sum Problem - GFG/maximum-sum-problem.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int solve(int n,vector<int> &dp){
+             if(n<=1){
+                return n;
+            }
+            if(dp[n] != -1){
+                return dp[n];
+            }
+            int sum = 0;
+            sum+=solve(n/2,dp);
+            sum+=solve(n/3,dp);
+            sum+=solve(n/4,dp);
+            return dp[n] = max(sum,n);
+        }
+        int maxSum(int n)
+        {
+            //code here.
+            vector<int> dp(n+1,-1);
+            return solve(n,dp);
+           
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.maxSum(n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
