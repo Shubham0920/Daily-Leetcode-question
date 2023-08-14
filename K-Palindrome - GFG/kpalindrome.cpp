f147db4b6b78b428561e7b9feb25c1cdb084c4a1
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2023-08-14 16:20:27 +0530

    Added solution - LeetHub

--- K-Palindrome - GFG/kpalindrome.cpp
+++ K-Palindrome - GFG/kpalindrome.cpp
@@ -1,36 +1,20 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
-
-bool is_k_palin(string s,int k);
-
-int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		string s ;
-		cin>>s;
-		int k;
-		cin>>k;
-		
-		cout<<is_k_palin(s,k)<<endl;
-	}
-	return 0;
-}
 // } Driver Code Ends
+// User function Template for C++
 
-
-/*You are required to complete this function*/
-bool is_k_palin(string s,int k)
-{
-//Your code here 
-        // int n = s.size();
+class Solution{
+public:
+    int kPalindrome(string s, int n, int k)
+    {
+        // code here
         string t = s;
         reverse(t.begin(),t.end());
-        int n = s.size();
+        // int n = s.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
@@ -44,4 +28,23 @@ bool is_k_palin(string s,int k)
         }
         int size = dp[n][n];
         return n-size <= k;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.kPalindrome(str, n, k)<<endl;
+    }
+    return 0;
 }
+// } Driver Code Ends
