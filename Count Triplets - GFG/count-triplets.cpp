619d82d8ace8deec9b64d6259bd87b7c8aeb722c
Shubham Varshney <64840036+Shubham0920@users.noreply.github.com>
2023-08-22 16:54:59 +0530

    Added solution - LeetHub

---
+++ Count Triplets - GFG/count-triplets.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+ 
+/* Link list node */
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node** head_ref, int new_data)
+{
+   
+    struct Node* new_node = new Node(new_data);
+    
+    new_node->next = (*head_ref);
+    (*head_ref)    = new_node;
+} 
+
+int countTriplets(struct Node* head, int x) ;
+
+/* Driver program to test count function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n , x ,i , num;
+        struct Node *head = NULL;
+        cin>>n>>x;
+        for(i=0;i<n;i++)
+        {
+            cin>>num;
+            push(&head,num);
+        }
+ 
+    /* Check the count function */
+    cout <<countTriplets(head, x)<< endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+int countTriplets(struct Node* head, int x) 
+{ 
+    // code here.
+    vector<int> ans;
+    struct Node* temp = head;
+    while(temp!= nullptr){
+        ans.push_back(temp->data);
+        temp = temp->next;
+    }
+    int cnt = 0;
+    for(int i=0;i<ans.size();i++){
+        int l = i+1;
+        int h = ans.size()-1;
+        while(l<h){
+            int sum = ans[i];
+            sum+=ans[l];
+            sum+=ans[h];
+            if(sum == x){
+                cnt++;
+                l++;h--;
+            }
+            else if(sum>x){
+                h--;
+            }
+            else{
+                l++;
+            }
+        }
+    }
+    return cnt;
+} 
